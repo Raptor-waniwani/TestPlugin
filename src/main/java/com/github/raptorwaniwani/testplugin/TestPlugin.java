66cdcb134797b827ed008db4825f49ac9e56599b
Raptor-waniwani <zenoanddoitu@gmail.com>
2024-11-17 23:47:28 +0900

    ba

--- src/main/java/com/github/raptorwaniwani/testplugin/TestPlugin.java
+++ src/main/java/com/github/raptorwaniwani/testplugin/TestPlugin.java
@@ -6,7 +6,7 @@ public final class TestPlugin extends Plugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("aaa");
     }
 
     @Override
